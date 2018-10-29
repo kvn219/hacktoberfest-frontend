d225f9a3161c390a32565f7f65016ba74ed991d0
Kevin Nguyen <kvn219@nyu.edu>
2018-10-29 10:21:50 -0400

    add random functions

--- src/components/AppCalculator.vue
+++ src/components/AppCalculator.vue
@@ -7,9 +7,7 @@
           <img class="tf_logo" src="../assets/tf_logo.svg.png">
         </div>
         <form action="">
-
           <h1>House Calculator</h1>
-          <button @click="nux">Button</button>
           <label for="rooms"># of Rooms</label>
           <input type="number" name="rooms" v-model.number="numRooms" />
           <br />
@@ -19,8 +17,8 @@
         </form>
         <div class="price">Predicted House Price: {{ price }}</div>
         <div>{{foo(this.numRooms, this.squareFootage)}}</div>
-        <div class="price">TF Predicted House Price: {{ predictedPrice }}</div>
-        <!-- <div>{{bar(this.numRooms, this.squareFootage)}}</div> -->
+        <button @click="randomFoo()" type="button">Random</button>
+        <div class="price">Random House Price: {{ randomNumber | displayNum}}</div>
       </div>
     </div>
   </div>
@@ -30,70 +28,44 @@
   import * as tf from '@tensorflow/tfjs'
   export default {
     name: 'AppCalulator',
+    filters: {
+      displayNum(x) {
+        return Math.round(x * 1000) / 1000
+      }
+    },
     data() {
       return {
         price: 0,
         squareFootage: 0,
         numRooms: 0,
         predictedPrice: 0,
+        randomNumber: 0.001,
       }
     },
     computed: {},
     methods: {
-      nux() {
-        tf.tensor([1, 2, 3, 4]).print();
+      randomFoo() {
+        return tf.tidy(() => {
+          const a = tf.variable(tf.scalar(Math.random()))
+          const b = tf.variable(tf.scalar(Math.random()))
+          const results = a.mul(b)
+          results.data().then(results => {
+            this.randomNumber = results[0]
+          })
+        })
       },
       foo(numRooms, squareFootage) {
         return tf.tidy(() => {
           const a = tf.tensor([Number.parseInt(numRooms)])
           const b = tf.variable(tf.scalar(Number.parseInt(squareFootage)))
-          const results = a.add(b);
+          const results = a.mul(b);
           results.data().then(results => {
             this.price = results[0]
           })
         });
       },
-      // bar(numRooms, squareFootage) {
-      //   return tf.tidy(() => {
-      //     const model = tf.sequential()
-      //     model.add(tf.layers.dense({
-      //       units: 1,
-      //       inputShape: [2]
-      //     }))
-
-      //     const xs = tf.tensor2d([
-      //       [1, 2],
-      //       [1, 3]
-      //     ], [2, 2])
-      //     const ys = tf.tensor2d([
-      //       [1],
-      //       [2]
-      //     ], [2, 1])
-
-      //     model.compile({
-      //       loss: 'meanSquaredError',
-      //       optimizer: 'sgd'
-      //     })
-
-      //     await model.fit(xs, ys, {epochs: 100, callbacks: {
-      //       onEpochEnd: async (epoch, log) => {
-      //         console.log(`Epoch ${epoch}: loss = ${log.loss}`
-      //         )}
-      //       }
-      //     })
-
-      //     model.predict(tf.tensor2d([5], [1, 1])).print();
-      //     // Train the model using the data.
-          // model.fit(xs, ys, {
-          //   epochs: 1
-          // }).then(() => {
-          //   const output = model.predict(tf.tensor2d([numRooms, squareFootage], [1, 2])).data()
-          //   return output
-          // })
-        // })
-      // }
-    }
   }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -197,22 +169,6 @@ input[type="number"]::-webkit-inner-spin-button,
 input[type="number"]::-webkit-outer-spin-button {
   -webkit-appearance: none;
   margin: 0;
-}
-
-.payment {
-  color: #000;
-  font-size: 24px;
-  text-align: center;
-  font-weight: bold;
-  transition: all 0.3s ease-in;
-  @media (max-width: 512px) {
-    font-size: 40px;
-    transition: all 0.3s ease-in;
-  }
-  @media (max-width: 400px) {
-    font-size: 32px;
-    transition: all 0.3s ease-in;
-  }
 }
 
 @media (max-height: 750px) {
